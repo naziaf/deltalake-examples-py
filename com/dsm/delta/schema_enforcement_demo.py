993b70927eb9a94d76dd9b1ffba0d93639dcb87e
naziaf <nazia.nasrat@gmail.com>
2021-07-17 09:20:04 -0700

    test

--- com/dsm/delta/schema_enforcement_demo.py
+++ com/dsm/delta/schema_enforcement_demo.py
@@ -39,8 +39,8 @@ if __name__ == '__main__':
 
     # Setup spark to use s3
     hadoop_conf = spark.sparkContext._jsc.hadoopConfiguration()
-    hadoop_conf.set("fs.s3a.access.key", app_secret["s3_conf"]["access_key"])
-    hadoop_conf.set("fs.s3a.secret.key", app_secret["s3_conf"]["secret_access_key"])
+    #hadoop_conf.set("fs.s3a.access.key", app_secret["s3_conf"]["access_key"])
+    #hadoop_conf.set("fs.s3a.secret.key", app_secret["s3_conf"]["secret_access_key"])
 
     delta_table_path = "s3a://" + app_conf["s3_conf"]["s3_bucket"] + "/schema_enforcement_delta"
 
